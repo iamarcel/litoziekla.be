ef706ccdded1d35d6bee9574b0f44ab99c5bff41
Marcel Samyn <dev@mrcl.io>
2018-01-24 17:03:47 +0100

    :star: feat: move Prins, add Lidmaatschap and Sponsoring pages

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,9 @@
 $(document).foundation();
+
+$(function () {
+  $('.open-member-signup-form').click(function (event) {
+    event.preventDefault();
+    document.cookie = "MCPopupClosed=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
+    require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us9.list-manage.com","uuid":"bcb2137b8063b24f1e828b419","lid":"0790fa5505"}) })
+  });
+});
